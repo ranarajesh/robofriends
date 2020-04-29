78c0df76799f23262a26561f91938d1a38a3d273
Rajesh Rana <rajesh.rana@zohomail.com>
2020-04-29 18:19:18 +0530

    Handle redux middleware on production mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,10 +7,11 @@ import { requestRobots } from "./robots/reducer";
 const logger = createLogger();
 
 const rootReducers = combineReducers({ searchRobots, requestRobots });
+let middleware = [thunkMiddleware];
+if (process.env.NODE_ENV === "development") {
+  middleware = [...middleware, logger];
+}
 
-const store = createStore(
-  rootReducers,
-  applyMiddleware(thunkMiddleware, logger)
-);
+const store = createStore(rootReducers, applyMiddleware(...middleware));
 
 export default store;
